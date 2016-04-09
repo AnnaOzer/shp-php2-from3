471c284e7375cca66537bda7271f5fdb444e862b
AnnaOzer <ozerskaya.ru@mail.ru>
2016-04-09 13:56:26 +0400

    + класс DbConnect соединяется с базой данных в конструкторе

--- functions/DbConnect.php
+++ functions/DbConnect.php
@@ -10,6 +10,17 @@ class DbConnect
     {
         $getConfig = new GetConfig;
         $this->config = $getConfig->getConfig();
+        if(!mysql_connect(
+            $this->config['db']['host'],
+            $this->config['db']['user'],
+            $this->config['db']['root']
+        )) {
+            die('Could not connect: ' . mysql_error());
+        };
+        if(!mysql_select_db($this->config['db']['dbname'])){
+            die('Could not connect to ' . $this->config['db']['dbname'] .  '  ' . mysql_error());
+        };
+
     }
 }
 
