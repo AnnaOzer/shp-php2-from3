a96c101c68c6d7f976d5380835d6ce0b08b7acaa
AnnaOzer <ozerskaya.ru@mail.ru>
2016-04-10 15:54:34 +0400

    + dependency injection внедрение зависимости - объект бд как свойство новости

--- models/Article.php
+++ models/Article.php
@@ -7,7 +7,33 @@ abstract class Article
     public $title;
     public $text;
 
+    private $db;
+    public function construct()
+    {
+        $this->db = new DbConnect();
+    }
+
     abstract public function getOne($params);
     abstract public function insertOne($params);
     abstract public function updateOne($params);
+}
+
+class NewsArticle
+    extends Article
+{
+
+    public function getOne($params)
+    {
+
+    }
+
+    public function insertOne($params)
+    {
+        // TODO: Implement insertOne() method.
+    }
+
+    public function updateOne($params)
+    {
+        // TODO: Implement updateOne() method.
+    }
 }
