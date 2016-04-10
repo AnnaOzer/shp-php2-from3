f11ca14cb549710f6d1147a1c9f48b822da5e9a8
AnnaOzer <ozerskaya.ru@mail.ru>
2016-04-10 15:40:45 +0400

    + абстрактный класс Article

--- models/Article.php
+++ models/Article.php
@@ -2,3 +2,12 @@
 
 require_once __DIR__ . '/../functions/DbConnect.php';
 
+abstract class Article
+{
+    public $title;
+    public $text;
+
+    abstract public function getOne($params);
+    abstract public function insertOne($params);
+    abstract public function updateOne($params);
+}
