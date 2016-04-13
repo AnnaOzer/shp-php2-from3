8a9d59ef0ec26077d7d0739e6347ddc95df6d931
AnnaOzer <ozerskaya.ru@mail.ru>
2016-04-13 18:22:43 +0400

    + буферизация в методе display или render класса view

--- other/tests/view.php
+++ other/tests/view.php
@@ -4,9 +4,24 @@ class View
 {
     public function display($template)
     {
-        include $template; // хотим вернуть из метода, не выводить пока
+        // начали буферизацию
+        ob_start();
+
+        // сделали что-то, что приведет к выводу на экран
+        include __DIR__ . '/' . $template;
+
+        // сохраняем в переменную что там в буфере накопилось
+        $ret = ob_get_contents();
+
+        // выключаем буферизацию
+        ob_end_clean();
+
+        // возвращаем из этого метода то что в буфере какопили и сохранили в переменную
+        return $ret;
     }
 }
 
 $view = new View();
-$view->display('template.html');
+$html = $view->display('template.html');
+
+echo $html;
