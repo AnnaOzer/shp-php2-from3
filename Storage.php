c8b97f9a2bcd07b6ef74d7d91c61566809e33824
AnnaOzer <ozerskaya.ru@mail.ru>
2016-04-13 19:53:07 +0400

    + в классе Storage методы current и next

--- Storage.php
+++ Storage.php
@@ -1,7 +1,7 @@
 <?php
 
 class Storage
-    implements Countable
+    implements Countable, Iterator
 {
     private $__data = [];
 
@@ -19,6 +19,51 @@ class Storage
     {
         return count($this->__data);
     }
+
+
+    public function current()
+    {
+        return current($this->__data);
+    }
+
+    public function next()
+    {
+        next($this->__data);
+    }
+
+    /**
+     * (PHP 5 &gt;= 5.0.0)<br/>
+     * Return the key of the current element
+     * @link http://php.net/manual/en/iterator.key.php
+     * @return mixed scalar on success, or null on failure.
+     */
+    public function key()
+    {
+        // TODO: Implement key() method.
+    }
+
+    /**
+     * (PHP 5 &gt;= 5.0.0)<br/>
+     * Checks if current position is valid
+     * @link http://php.net/manual/en/iterator.valid.php
+     * @return boolean The return value will be casted to boolean and then evaluated.
+     * Returns true on success or false on failure.
+     */
+    public function valid()
+    {
+        // TODO: Implement valid() method.
+    }
+
+    /**
+     * (PHP 5 &gt;= 5.0.0)<br/>
+     * Rewind the Iterator to the first element
+     * @link http://php.net/manual/en/iterator.rewind.php
+     * @return void Any returned value is ignored.
+     */
+    public function rewind()
+    {
+        // TODO: Implement rewind() method.
+    }
 }
 
 $st = new Storage();
@@ -28,3 +73,8 @@ $st->foo3 = 'bar3';
 
 echo $st->foo; // bar
 echo $st->count(); // 3
+
+echo $st->current(); // bar
+$st->next();
+echo $st->current(); // bar2
+
