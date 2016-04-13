4706df481d42aa4286e14a83d9558d24d55298bc
AnnaOzer <ozerskaya.ru@mail.ru>
2016-04-13 19:41:36 +0400

    + класс Storage реализует системный интерфейс Countable

--- Storage.php
+++ Storage.php
@@ -1,6 +1,7 @@
 <?php
 
 class Storage
+    implements Countable
 {
     private $__data = [];
 
@@ -13,8 +14,17 @@ class Storage
     {
         return $this->__data[$key];
     }
+
+    public function count()
+    {
+        return count($this->__data);
+    }
 }
 
 $st = new Storage();
 $st->foo = 'bar';
-echo $st->foo;
+$st->foo2 = 'bar2';
+$st->foo3 = 'bar3';
+
+echo $st->foo; // bar
+echo $st->count(); // 3
