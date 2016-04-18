524a611de511723df74cfbeb3394ab381adeda66
AnnaOzer <ozerskaya.ru@mail.ru>
2016-04-18 14:51:08 +0400

    + index.php пример автозагрузки 2 классов

--- index.php
+++ index.php
@@ -1,7 +1,17 @@
 <?php
 
-require_once __DIR__ . '/models/Article.php';
+// функция будет вызвана автоматически, когда встретит класс, который не был ранее загружен
+function __autoload($class)
+{
+    // логика поиска файлов для класса
+    require 'classes/' . $class . '.php';
+}
+
+/*
 require_once __DIR__ . '/classes/View.php';
+*/
+
+require_once __DIR__ . '/models/Article.php';
 
 $newsArticle = new NewsArticle();
 
