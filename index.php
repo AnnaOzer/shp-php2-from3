0b4cb8dd7a0a1aa72c5e4351f70c9afb8fe7127d
AnnaOzer <ozerskaya.ru@mail.ru>
2016-04-18 15:24:07 +0400

    + __autoload и загрузка модели помещена в boot.php

--- index.php
+++ index.php
@@ -1,17 +1,6 @@
 <?php
 
-// функция будет вызвана автоматически, когда встретит класс, который не был ранее загружен
-function __autoload($class)
-{
-    // логика поиска файлов для класса
-    require 'classes/' . $class . '.php';
-}
-
-/*
-require_once __DIR__ . '/classes/View.php';
-*/
-
-require_once __DIR__ . '/models/Article.php';
+require 'boot.php';
 
 $newsArticle = new NewsArticle();
 
