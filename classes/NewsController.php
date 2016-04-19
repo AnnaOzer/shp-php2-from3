249413a7eda65b18d6e183dec227ce1772f3b10a
AnnaOzer <ozerskaya.ru@mail.ru>
2016-04-19 17:47:31 +0400

    + роутер и фронтконтроллер

--- classes/NewsController.php
+++ classes/NewsController.php
@@ -1,7 +1,5 @@
 <?php
 
-
-
 class NewsController
     extends AController
 {
--- index.php
+++ index.php
@@ -2,5 +2,18 @@
 
 require 'boot.php';
 
-$controller = new NewsController();
-$controller->action('all');
+// r=news/all
+
+// начало роутера
+$route = $_GET['r'];
+$routeParts = explode('/', $route);
+$controllerClassName = ucfirst($routeParts[0]).'Controller';
+$actionName= $routeParts[1];
+// конец роутера
+
+// начало фронтконтроллера
+$controller = new $controllerClassName;
+$controller->action($actionName);
+// конец фронтконтроллера
+
+// смотреть на http://shp-php2-triple/?r=news/all
