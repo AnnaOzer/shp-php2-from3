d4da660455c911f813eef399ea6da43d93859fcb
AnnaOzer <ozerskaya.ru@mail.ru>
2016-04-21 20:18:12 +0400

    + пример вывода статического свойства предка

--- other/tests/orm.php
+++ other/tests/orm.php
@@ -1,15 +1,23 @@
 <?php
 
 abstract class Model {
-    static protected $table;
-    static public function getTableName() {
+
+    // мы считаем, что это свойство должно быть у любой модели
+    // в какой таблице в базе у нас эта модель
+    // свойство объявлено статическим, принадлежит классу а не объектам этого класса
+    static protected $table='123';
+
+    // статический метод возвращает название таблицы
+    static function getTableName() {
+
+        // ключевое слово self и :: для доступа к классу - свойствам и методам
         return self::$table;
     }
 }
 
-class Article extends Model {
-    static protected $table = 'articles';
+class News extends Model {
+    static protected $table = 'news';
 }
-echo Article::getTableName();
+echo News::getTableName();
 
-// ничего не выводит
+// выводит 123
