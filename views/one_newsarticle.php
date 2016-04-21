e71670da214ef0f7f3307aa2a33b6ccd8d492f63
AnnaOzer <ozerskaya.ru@mail.ru>
2016-04-21 11:53:17 +0400

    ! завершено дз 4 (+)

---
+++ views/one_newsarticle.php
@@ -0,0 +1,10 @@
+<article>
+    <header>
+        <h1><?php echo $one_newsarticle['title']; ?></h1>
+
+        <h3><?php echo $one_newsarticle['author']; ?></h3>
+    </header>
+    <div><?php echo $one_newsarticle['text']; ?>
+        <a href="/?r=news/edit&id=<?php echo $one_newsarticle['id']; ?>">[Редактировать]</a>
+    </div>
+</article>
