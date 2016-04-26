3b30f66a0529b086c2b37738d67b050fd60e2888
AnnaOzer <ozerskaya.ru@mail.ru>
2016-04-26 11:39:30 +0400

    + пример раннего статического связывания

--- other/tests/orm.php
+++ other/tests/orm.php
@@ -2,22 +2,18 @@
 
 abstract class Model {
 
-    // мы считаем, что это свойство должно быть у любой модели
-    // в какой таблице в базе у нас эта модель
-    // свойство объявлено статическим, принадлежит классу а не объектам этого класса
     static protected $table='123';
 
-    // статический метод возвращает название таблицы
-    static function getTableName() {
-
-        // ключевое слово self и :: для доступа к классу - свойствам и методам
-        return self::$table;
+    static function getTableName()
+    {
+         return self::$table;
     }
 }
 
-class News extends Model {
-    static protected $table = 'news';
+class News extends Model
+{
+    static public $table = 'news';
 }
-echo News::getTableName();
 
-// выводит 123
+echo News::getTableName();
+echo News::$table;
