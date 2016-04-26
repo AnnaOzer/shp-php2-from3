6e17e4320ee9ace4f04c41ff955045be2b32cbcc
AnnaOzer <ozerskaya.ru@mail.ru>
2016-04-26 15:37:33 +0400

    ! Exception - пример пойманного исключения (+)

--- other/tests/orm.php
+++ other/tests/orm.php
@@ -17,12 +17,17 @@ abstract class Model {
 
     static function findAll()
     {
-        $sql = 'SELECT * FROM ' . static::$table;
-        $dbh = static::getConnection();
-        $sth = $dbh->prepare($sql);
-        $sth->setFetchMode(PDO::FETCH_CLASS, get_called_class());
-        $sth->execute();
-        return $sth->fetchAll();
+        try {
+            $sql = 'SELECT * FROM ' . static::$table;
+            $dbh = static::getConnection();
+            $sth = $dbh->prepare($sql);
+            $sth->setFetchMode(PDO::FETCH_CLASS, get_called_class());
+            $sth->execute();
+            return $sth->fetchAll();
+        } catch (Exception $e) {
+            echo $e->getMessage();
+            die;
+        }
 
     }
 }
