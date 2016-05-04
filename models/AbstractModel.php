41ab79b78a557115815e89e7710eeea37d970f53
AnnaOzer <ozerskaya.ru@mail.ru>
2016-05-04 15:51:35 +0400

    + метод save() и св-во columns в абстр. модели

--- models/AbstractModel.php
+++ models/AbstractModel.php
@@ -3,6 +3,7 @@
 abstract class AbstractModel
 {
     static protected $table;
+    static protected $columns = [];
 
     static function findAll()
     {
@@ -18,4 +19,39 @@ abstract class AbstractModel
         return $connection->toExecute($sql, [':id' => $id])->fetch();
     }
 
+    public function save()
+    {
+        $tokens = [];
+        $values = [];
+        foreach (static::$columns as $column) {
+            $tokens[] = ':' . $column;
+            $values[':' . $column] = $this->$column;
+        }
+
+        $dbh = static::getConnection();
+
+        if(!isset($this->id)) {
+
+            $sql = 'INSERT INTO ' . static::$table . '
+            (' . implode(', ', static::$columns) . ')
+             VALUES
+            (' . implode(', ', $tokens). ')';
+            $sth = $dbh->prepare($sql);
+            $sth->execute($values);
+            $this->id = $dbh->lastInsertId();
+
+        } else {
+
+            $columns = [];
+            foreach (static::$columns as $column) {
+                $columns[] = $column . '=:' . $column;
+            }
+            $sql = 'UPDATE ' . static::$table . '
+            SET ' . implode(',' , $columns) . '
+            WHERE id=:id
+            ';
+            $sth = $dbh->prepare($sql);
+            $sth->execute([':id' =>$this->id] + $values); // массивы в php можно складывать
+        }
+    }
 }
